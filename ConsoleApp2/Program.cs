9dde051b52e0e56e1dae27aad72737733013afba
John Doe <john.doe@gmail.com>
2019-07-10 10:22:08 -0300

    Mostrando entendimento sobre arrays

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
@@ -10,7 +11,29 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello git");
+            string[] nameList = new string[2] { "valor 1", "valor 2 " };
+            foreach (var item in nameList)
+                Console.WriteLine(item);
+
+            string[] dateList = new string[10000];
+            DateTime firstTime = DateTime.Now;
+
+            for (int i = 0; i < dateList.Length; i++)
+            {
+                dateList[i] = DateTime.Now.ToString("yyyy-MM-dd-hh:mm:ssfff");
+                Console.WriteLine(dateList[i]);
+            }
+
+                Console.WriteLine((DateTime.Now - firstTime).ToString());
+
+            //foreach (var item in dateList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            Console.ReadKey();
+
         }
     }
 }
