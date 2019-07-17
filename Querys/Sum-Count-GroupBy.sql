d4cfb69e5f625c1610d1ec8f3a96650b6683c2cd
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 15:21:39 -0300

    Visto Sum/Count/GroupBy

---
+++ Querys/Sum-Count-GroupBy.sql
@@ -0,0 +1,8 @@
+﻿select
+	Nome,
+	Materia,
+	(SUM(Nota)/ COUNT(Nota)) as 'Media', --soma as notas, conta as provas e faz a media delas
+	COUNT(Nota) as 'provas' -- conta e nomeia o numero de provas realizadas
+from NotaALuno
+GROUP BY Nome, Materia -- agrupa por identificadores comuns
+order by Nome asc
