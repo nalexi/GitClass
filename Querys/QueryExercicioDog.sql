0fe7196b7decffdf4d0b3b4585cee69486948242
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 17:28:19 -0300

    Exercicio query dog

---
+++ Querys/QueryExercicioDog.sql
@@ -0,0 +1,21 @@
+﻿--select 
+--	TipoDog,
+--	TotalVendas,
+--	COunt(MONTH(dataVenda)) as 'data venda'
+--from TabelaDog
+--group by TipoDog, TotalVendas, MONTH(DataVenda)
+
+
+
+
+
+select 
+
+	TipoDog,
+	TotalVendas,
+	(MONTH(DataVenda)) as 'soma vendas',
+
+	DataVenda
+
+from TabelaDog
+group by TipoDog, TotalVendas, DataVenda
