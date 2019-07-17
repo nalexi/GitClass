e5f2c7f000fec957d962b13f2dc55b6a48f6e089
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 16:32:29 -0300

    select feito com a soma das unidades vendidas / pela data da venda - SUM/COUNT/YEAR/GROUP BY/WHERE(YEAR(DATA)

---
+++ Querys/SelectVendaComAno.sql
@@ -0,0 +1,9 @@
+﻿Select 
+	Modelo,
+	Ano,
+	SUM(NumeroDeVenda)/COUNT(DataVenda) as 'media',
+	YEAR(datavenda) as 'ano'
+	from TabelaCarros
+	Where YEAR(DataVenda) = 2015
+	GROUP BY modelo, ano, YEAR(DataVenda);
+
