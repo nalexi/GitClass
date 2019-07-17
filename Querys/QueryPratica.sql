f6157f43bd2105d51715393dfa61610136238c13
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 14:57:28 -0300

    pratica query

---
+++ Querys/QueryPratica.sql
@@ -0,0 +1,13 @@
+﻿select 
+Nome,
+Materia,
+((Nota1 + Nota2 + Nota3 + Nota4 + Nota5 + Nota6)/6) as 'Media',
+IIF(((Nota1 + Nota2 + Nota3 + Nota4 + Nota5 + Nota6)/6) >= 5, 'aprovado','reprovado')as 'Status'
+from Diario1 where id = 3;
+
+--insert into Diario1
+--(Nome, Materia, Nota1, Nota2 ,Nota3 ,Nota4, Nota5 ,Nota6)
+--Values
+--('Raul','facil', 8,6,8,8,6,9)
+--go
+--select * from Diario1
