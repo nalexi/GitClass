cf765ea9c766a1f42ac1fa03d600fecdebe8dabf
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 13:58:40 -0300

    Query utilizando BL e LV coun ID livro, locado, sim, nao

---
+++ Querys/query BL e LV.sql
@@ -0,0 +1,15 @@
+﻿--select 
+--	LV.Id,
+--	LV.Nome,
+--	IIF(LV.Alocado, 'sim','Não') as 'Locado',
+--	BL.Nome
+--	from Biblioteca BL
+--	inner join on Biblioteca.Id = Livro.Biblioteca
+
+select 
+		BL.Nome,
+		COUNT(LV.id) as  'QuantidadeDeLivros'
+	from Biblioteca BL
+	inner join Livro LV on BL.id = LV.biblioteca
+	--inner join Usuario US on LV.UsuInc = US.ID
+	group by Bl.Nome 
