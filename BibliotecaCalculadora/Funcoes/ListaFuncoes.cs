34660040e167d21b1cdaeb344d68bf338857a232
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:21:45 -0300

    Commit de biblioteca com referencia em outro projeto

---
+++ BibliotecaCalculadora/Funcoes/ListaFuncoes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadora.Funcoes
+{
+    public class ListaFuncoes
+    {
+        public int Adicao(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int Subtracao(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public int Multiplicacao(int valor1, int valor2)
+        {
+            return valor1* valor2;
+        }
+        public int Divisao(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
---
+++ BibliotecaCalculadoraRef/Program.cs
@@ -0,0 +1,28 @@
+﻿using BibliotecaCalculadora.Funcoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadoraRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaFuncoes funcoes = new ListaFuncoes();
+            //Console.WriteLine("digite os valores desejados");
+            //
+            //Console.WriteLine(funcoes.Adicao(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+            Console.WriteLine(funcoes.Adicao(2, 2));
+            Console.WriteLine(funcoes.Subtracao(2, 2));
+            Console.WriteLine(funcoes.Multiplicacao(2, 2));
+            Console.WriteLine(funcoes.Divisao(2, 2));
+
+            Console.ReadKey();
+         
+
+        }
+    }
+}
