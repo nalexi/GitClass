d0b68808b94965ff3beccbbff317b666bfe09336
hbsis <hbsis@GREEN02X>
2019-07-24 11:58:24 -0300

    Criação de lista organizada com classe

---
+++ CriacaoDeListaOrganizado/Listas/CarregaListas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeListaOrganizado.Listas
+{
+    class CarregaListas
+    {
+
+        string[] lista;
+
+        public CarregaListas()
+        {
+            lista = new string[10];
+            CarregarLista();
+        }
+
+       
+        public void CarregarLista()
+        {
+            for (int i = 0; i < lista.Length; i++)
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+        }
+
+        public void MostrarLista()
+        {
+            foreach (var item in lista)
+                Console.WriteLine(item);
+            Console.ReadKey();
+
+        }
+
+        public void ApagaLista()
+        {
+            for (int i = 0; i < lista.Length; i++)
+                lista[i] = ".";//string.Empty;//Aqui limpamos o nosso valor dentro da coleção
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CriacaoDeListaOrganizado/Program.cs
@@ -0,0 +1,30 @@
+﻿using CriacaoDeListaOrganizado.Listas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeListaOrganizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testeLista = new CarregaListas();
+
+            testeLista.MostrarLista();
+
+            testeLista.ApagaLista();
+         
+            testeLista.MostrarLista();
+
+            testeLista.CarregarLista();
+            
+
+            
+
+        }
+
+    }
+}
