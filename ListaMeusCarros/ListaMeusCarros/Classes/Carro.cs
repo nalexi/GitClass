daeeeabe8bd28044b4a44086f429217db10cbd4e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 17:20:51 -0300

    Exercicio Lista_meus_carros

---
+++ ListaMeusCarros/ListaMeusCarros/Classes/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaMeusCarros.Classes
+{
+    public class Carro
+    {
+        public string Modelo { get; set; }
+        public string Ano { get; set; }
+        public string Placa { get; set; }
+        public int PulsoesPulgmaticos { get; set; }
+
+
+    }
+}
---
+++ ListaMeusCarros/ListaMeusCarros/Program.cs
@@ -0,0 +1,75 @@
+﻿using ListaMeusCarros.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaMeusCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Carro> meusCarros = new List<Carro>();
+
+
+            while (true)
+            {
+                switch (MostrarMenu())
+                {
+                    case 1:
+                        Console.Clear();
+
+                        Console.WriteLine("Adicione um veiculo");
+
+                        meusCarros.Add(new Carro()
+                        {
+                            Modelo = RetornarValores("Modelo"),
+                            Ano = RetornarValores("Ano"),
+                            Placa = RetornarValores("placa"),
+                            PulsoesPulgmaticos = Convert.ToInt32(RetornarValores("Pulsoes Pugmaticos"))
+                        });
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Lista de carros");
+                        foreach (Carro item in meusCarros)
+                        {
+                            Console.WriteLine($"Carro: {item.Modelo}, de ano: {item.Ano}, placa: {item.Modelo} e " +
+                                $"{item.PulsoesPulgmaticos} pulsoes pugmaticos");
+                        }
+
+                        break;
+
+                    case 3:
+
+                        return;
+
+                    default:
+                        Console.WriteLine("Digite um valor valido");
+                        break;
+                }
+            }
+        }
+
+        public static string RetornarValores(string valor)
+        {
+            Console.WriteLine($"Digite o valor para o campo {valor}");
+            return Console.ReadLine();
+        }
+
+        public static int MostrarMenu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("Escolha opção");
+            Console.WriteLine("1 - Adicionar Veiculos");
+            Console.WriteLine("2 - Listar veículos");
+            Console.WriteLine("3 - sair");
+
+            var opcao = Convert.ToInt32(Console.ReadLine());
+            return opcao;
+        }
+    }
+}
